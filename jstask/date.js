aaad38d11fef025de96c135ea504a5559992da37
MansurovF <112488802+MansurovF@users.noreply.github.com>
2022-11-18 17:18:47 +0400

    Add files via upload

---
+++ jstask/date.js
@@ -0,0 +1,21 @@
+// var date= new Date();
+// if(date.getHours()>=6&&date.getHours()<12){
+//     document.body.style.backgroundColor='blue';
+//     alert('Sabahın xeyir')
+// }
+// else if(date.getHours()>=12&&date.getHours()<18){
+//     document.body.style.backgroundColor='orange';
+//     alert('Günortan xeyir')
+// }
+// else if(date.getHours()>=18&&date.getHours()<24){
+//     document.body.style.backgroundColor='black';
+//     alert('Axşamın xeyir')
+// }
+// else if(date.getHours()>=0&&date.getHours()<5){
+//     document.body.style.backgroundColor='grey';
+//     alert('Beyaz geceler')
+// }
+
+// var r = 5
+// var s = Math.pow(r,2)*Math.PI
+// console.log(s)
